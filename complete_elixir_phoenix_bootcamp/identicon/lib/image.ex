fb42b2484067e1f7dc507052827662b39f6429f3
Marius Butuc <marius.butuc@gmail.com>
2016-11-29 12:45:41 -0500

    Building the pixel map

--- complete_elixir_phoenix_bootcamp/identicon/lib/image.ex
+++ complete_elixir_phoenix_bootcamp/identicon/lib/image.ex
@@ -1,7 +1,8 @@
 defmodule Identicon.Image do
   defstruct [
-    colour: nil,
-    grid:   nil,
-    hex:    nil,
+    colour:    nil,
+    grid:      nil,
+    hex:       nil,
+    pixel_map: nil,
   ]
 end
